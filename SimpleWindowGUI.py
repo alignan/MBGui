e6faa3d7894f3c8f345526f323ada48651e9757e
Antonio Lignan <alinan@zolertia.com>
2014-08-27 12:37:35 +0200

    Added license headers and updated about box

--- SimpleWindowGUI.py
+++ SimpleWindowGUI.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
 
+# Modbus GUI tool
+#
+# Copyright (C) 2014, Antonio Liñan,
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#  3. The names of its contributors may not be used to endorse or promote
+#     products derived from this software without specific prior written
+#     permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Any feedback is very welcome.
+# https://github.com/alignan/MBGui
+
 __author__ = "Antonio Lignan"
 
 # // TODO: Reduce repeated code: create classes for buttons, labels, etc... this was done on the go!
@@ -24,12 +59,13 @@ from xml.dom import minidom
 # To periodically execute a request
 # import time, threading
 
-DEBUG_MB = 1
+DEBUG_MB = 0
 
 if DEBUG_MB:
     import logging
 
-ABOUT_INFO = "Hello world"
+ABOUT_INFO = "Silly tool put together by:\nAntonio Lignan, 2014\nPython Software Foundation License (PSFL)"
+VERSION_INFO = "Release version v1.0.0, https://github.com/alignan/MBGui"
 
 # Name of the default memory map XML file
 mmap_abs_path = os.path.dirname(os.path.abspath(__file__)) + "\mmap.xml"
@@ -290,8 +326,7 @@ class SimpleWindowGUI:
 
     # The classic about popup window
     def display_about(self):
-        # TODO: get somehow the firmware version, date, etc, license!!!
-        tkMessageBox.showinfo("About MBGui", ABOUT_INFO)
+        tkMessageBox.showinfo("About MBGui", ABOUT_INFO + '\n' + VERSION_INFO)
 
     # Load a memory map file
     def load_memory_map(self):
